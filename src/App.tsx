2c60c2db7e960634f442d30dbedeb77732ec8b11
while-true-if <129566635+while-true-if@users.noreply.github.com>
2024-11-22 16:20:55 +0900

    Changed introduction messages for cryptography

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     <>
-      <p className='green'>abc:2138900377818567013378428013918</p>
-      <p className='green'>dev:92614055269634911263963702143515712631719612603547267191</p>
-      <p className='green'>59623742807209209605624220260196145625260967092780961964968924496743848141562113152227876196187567479216962180788564356968452</p>
-      <h2 className='water'>掲示板</h2>
+      <p>I think it is difficult for you to solve this problem.</p>
+      <p>You mustn't be able to understand the meaning of this strange strings now, even in the future of course.</p>
+      <p>Solve this problem if you can.</p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p className='green'>abc: 2138900377818567013378428013918</p>
+      <p className='green'>dev: 92614055269634911263963702143515712631719612603547267191</p>
+      <p className='green'>???: 59623742807209209605624220260196145625260967092780961964968924496743848141562113152227876196187567479216962180788564356968452</p>
+      <h2 className='water'>bulletin board</h2>
       <p className='green'>important:1324</p>
-      <p className='green'>some delite</p>
+      <p className='green'>some delete</p>
     </>
   )
 }
---
+++ src/abcdefghijklmnopqrstuvwxyz.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function Abc() {
+
+  return (
+    <>
+      <p>Hello from abc</p>
+    </>
+  )
+}
+
+export default Abc
